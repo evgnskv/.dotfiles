b88a331db67c0da4ae882b6246adab28e482d11b
evgnskv <evgnskv@pm.me>
2025-07-14 13:40:48 +0900

    swap solarized for tokyonight

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -1,11 +1,11 @@
 return { 
-  "maxmx03/solarized.nvim", 
-  name = "solarized", 
+  "folke/tokyonight.nvim", 
+  name = "tokyonight", 
   priority = 1000, 
   lazy = false,
 
   config = function()
     vim.o.termguicolors = true
-    vim.cmd.colorscheme 'solarized'
+    vim.cmd.colorscheme 'tokyonight'
   end
 }
