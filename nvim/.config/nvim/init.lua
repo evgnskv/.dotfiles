83697595d4e457ecb95119a439d1f4bd6d907436
evgnskv <evgnskv@pm.me>
2025-07-14 12:17:51 +0900

    add neo-tree

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -27,7 +27,17 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
     { "maxmx03/solarized.nvim", name = "solarized", priority = 1000, lazy = false },
     { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
-    { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" }
+    { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+      },
+      lazy = false,
+    }
 }
 local opts = {}
 
@@ -57,3 +67,5 @@ config.setup({
   },
   indent = { enable = true },
 })
+
+ vim.keymap.set("n", "<leader>e", "<Cmd>Neotree reveal<CR>")
