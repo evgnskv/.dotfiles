20231b53edce466beb21004b5ae6c25866531b17
Evgeniy Noskov <ts-evgeniy.noskov@rakuten.com>
2025-08-22 11:07:24 +0900

    rm nvim lsp preinstall

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,14 +9,6 @@ return {
 
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "bashls",   -- LSP https://github.com/bash-lsp/bash-language-server
-                    "lua_ls",   -- LSP https://github.com/LuaLS/lua-language-server
-                    "pylsp",    -- LSP https://github.com/python-lsp/python-lsp-server
-                    "ruby_lsp", -- LSP https://github.com/Shopify/ruby-lsp
-                    "helm_ls",  -- LSP https://github.com/mrjosh/helm-ls
-                    "sqls",     -- LSP https://github.com/sqls-server/sqls
-                },
                 automatic_enable = true,
             })
         end,
