bcc02e66205db9a52b51c3849d0a2a0a6aed76a8
Evgeniy Noskov <evgnskv@pm.me>
2025-08-31 17:22:34 +0900

    start neotree closed

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -3,21 +3,51 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim", -- This is the missing dependency
+    "MunifTanjim/nui.nvim",
   },
+  init = function()
+    -- do nothing
+  end,
+  keys = function()
+  local find_buffer_by_type = function(type)
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+      if ft == type then return buf end
+    end
+    return -1
+  end
+  local toggle_neotree = function(toggle_command)
+    if find_buffer_by_type "neo-tree" > 0 then
+      require("neo-tree.command").execute { action = "close" }
+    else
+      toggle_command()
+    end
+  end
+
+  return {
+    {
+      "<leader>e",
+      function()
+        toggle_neotree(function() require("neo-tree.command").execute { action = "focus", reveal = true, dir = vim.uv.cwd() } end)
+      end,
+      desc = "Toggle Explorer (cwd)",
+    },
+    {
+      "<leader>E",
+      function()
+        toggle_neotree(function() require("neo-tree.command").execute { action = "focus", reveal = true } end)
+      end,
+      desc = "Toggle Explorer (root)",
+    },
+  }
+end,
   opts = {
     filesystem = {
       filtered_items = {
-        visible = true,
+        visible = false,
         show_hidden_count = true,
         hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          -- '.git',
-          -- '.DS_Store',
-          -- 'thumbs.db',
-        },
-        never_show = {},
+        hide_gitignored = false,
       },
     },
   },
