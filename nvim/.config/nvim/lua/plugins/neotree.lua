1e1620e88fe8fed3007b0b7d5cb204484b502e8c
Evgeniy Noskov <ts-evgeniy.noskov@rakuten.com>
2025-09-01 17:17:53 +0900

    cleanup neotree explorer command

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -5,9 +5,11 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+
   init = function()
     -- do nothing
   end,
+
   keys = function()
   local find_buffer_by_type = function(type)
     for _, buf in ipairs(vim.api.nvim_list_bufs()) do
@@ -16,6 +18,7 @@ return {
     end
     return -1
   end
+
   local toggle_neotree = function(toggle_command)
     if find_buffer_by_type "neo-tree" > 0 then
       require("neo-tree.command").execute { action = "close" }
@@ -28,18 +31,16 @@ return {
     {
       "<leader>e",
       function()
-        toggle_neotree(function() require("neo-tree.command").execute { action = "focus", reveal = true, dir = vim.uv.cwd() } end)
+        toggle_neotree(function() require("neo-tree.command").execute {
+            action = "focus",
+            reveal = true,
+            dir = vim.uv.cwd()
+          } end)
       end,
       desc = "Toggle Explorer (cwd)",
     },
-    {
-      "<leader>E",
-      function()
-        toggle_neotree(function() require("neo-tree.command").execute { action = "focus", reveal = true } end)
-      end,
-      desc = "Toggle Explorer (root)",
-    },
   }
+
 end,
   opts = {
     filesystem = {
